e01e81d4032643fe6bed5ad78d47117b7ec2222a
LucasR65 <158263593+LucasR65@users.noreply.github.com>
2025-04-10 11:55:13 +0200

    Ajout navigation bar

--- app/src/main/java/com/example/collabasket/MainActivity.java
+++ app/src/main/java/com/example/collabasket/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.collabasket;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.nav_list:
+                    startActivity(new Intent(this, ListActivity.class));
+                    return true;
+                case R.id.nav_inventory:
+                    startActivity(new Intent(this, InventoryActivity.class));
+                    return true;
+                case R.id.nav_group:
+                    startActivity(new Intent(this, GroupActivity.class));
+                    return true;
+                default:
+                    return false;
+            }
         });
     }
 }
